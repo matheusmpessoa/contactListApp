8a5a5431a8b5ef824f536b5489f26f9b8f3da0b5
Matheus Pessoa <matheusmpessoa17@gmail.com>
2016-06-01 16:19:41 -0300

    Validação de input para telefone

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -55,3 +55,43 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         return element.name.match(/^Ma/) ? true : false;
     };
 }]);
+
+/* Diretiva para habilitar apenas números no input */
+myApp.directive('validInput', function () {
+    return {
+        require: '?ngModel',
+        scope: {
+            "inputPattern": '@'
+        },
+
+        link: function (scope, element, attrs, ngModelCtrl) {
+            var regexp = null;
+
+            if (scope.inputPattern !== undefined) {
+                regexp = new RegExp(scope.inputPattern, "g");
+            }
+            if (!ngModelCtrl) {
+                return;
+            }
+
+            ngModelCtrl.$parsers.push(function (val) {
+                if (regexp) {
+                    var clean = val.replace(regexp, '');
+                    if (val !== clean) {
+                        ngModelCtrl.$setViewValue(clean);
+                        ngModelCtrl.$render();
+                    }
+                    return clean;
+                } else {
+                    return val;
+                }
+            });
+
+            element.bind('keypress', function (event) {
+                if (event.keyCode === 32) {
+                    event.preventDefault();
+                }
+            });
+        }
+    }
+});
